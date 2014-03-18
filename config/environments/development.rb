a5d5fe64e39aff4b21579373a14e5c308764e439
Jan Bator <jan.bator@gmail.com>
2014-03-18 20:31:08 +0100

    Translated sign in view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,4 +15,19 @@ Tales::Application.configure do
   config.active_record.migration_error = :page_load
 
   config.assets.debug = true
+  
+  # debug tłumaczeń
+  #  I18n.class_eval do
+  #    class << self
+
+  #      def translate_with_output(*args)
+  #        puts "Translation called with arguments: "
+  #        args.each{|a| puts a.inspect}
+  #        translate_without_output(*args)
+  #      end
+
+  #      alias_method_chain :translate, :output
+  #      alias_method :t, :translate
+  #    end
+  #  end
 end
