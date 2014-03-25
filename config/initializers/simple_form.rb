35f002a9d2b24a2e98ecb34b967f0d62908366e3
Jan Bator <jan.bator@gmail.com>
2014-03-25 19:16:39 +0100

    Form errors are bootstrap friendly.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -7,51 +7,27 @@ SimpleForm.setup do |config|
   config.button_class = "btn btn-default"
   config.browser_validations = false
   
-  config.wrappers :bootstrap, :tag => 'div', :class => 'form-group', :error_class => 'error' do |b|
+  config.wrappers :bootstrap, tag: 'div', class: 'form-group',
+                  error_class: 'has-error' do |b|
     b.use :html5
     b.use :label
     b.use :placeholder
     b.use :input
-    b.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-    b.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
-  end
-
-  config.wrappers :prepend, :tag => 'div', :class => "form-group", :error_class => 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'col-sm-8' do |input|
-      input.wrapper :tag => 'div', :class => 'input-prepend' do |prepend|
-        prepend.use :input
-      end
-      input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-    end
-  end
-
-  config.wrappers :append, :tag => 'div', :class => "form-group", :error_class => 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'col-sm-6' do |input|
-      input.wrapper :tag => 'div', :class => 'input-append' do |append|
-        append.use :input
-      end
-      input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-    end
+    b.use :error, wrap_with: { tag: 'div', class: 'help-block' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'help-block' }
   end
   
-  config.wrappers :inline_checkbox, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+  config.wrappers :inline_checkbox, tag: 'div', class: 'control-group',
+                  error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
-      ba.wrapper :tag => 'label', :class => 'checkbox' do |bb|
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.wrapper tag: 'label', class: 'checkbox' do |bb|
         bb.use :input
         bb.use :label_text
       end
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+      ba.use :error, wrap_with: { tag: 'div', class: 'help-block' }
+      ba.use :hint, wrap_with: { tag: 'div', class: 'help-block' }
     end
   end
   
