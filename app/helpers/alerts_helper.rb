ffc2717130b66ccf50b49da17d5c5fae6e066e8f
Jan Bator <jan.bator@gmail.com>
2014-03-25 21:10:40 +0100

    Access denied translated.

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -3,6 +3,7 @@ module AlertsHelper
     case kind.to_s
       when "alert" then "alert-danger"
       when "notice" then "alert-success"
+      when "warning" then "alert-warning"
       else raise "Invalid alert kind"
     end
   end
