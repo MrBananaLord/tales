aaed11213ef85ea84427771350e485adbac76749
Jan Bator <jan.bator@gmail.com>
2014-03-26 09:19:54 +0100

    Association between nodes (successors, predecessors).

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,4 +1,7 @@
 class Node < ActiveRecord::Base
   belongs_to :game
-  has_many :children_edges, foreign_key: "tail_id", class_name: "Edge"
+  has_many :children_edges, foreign_key: :tail_id, class_name: "Edge"
+  has_many :successors, through: :children_edges, source: :head
+  has_many :parent_edges, foreign_key: :head_id, class_name: "Edge"
+  has_many :predecessors, through: :parent_edges, source: :tail
 end
