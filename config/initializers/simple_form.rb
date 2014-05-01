ba3646c6ccf37a7ac6dabd0db3981573ebef17ee
Jan Bator <jan.bator@gmail.com>
2014-05-01 18:55:32 +0200

    Hacked SimpleForm for bootstrap compatibility (file input shouldn't have 'form-control' class.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,3 +1,28 @@
+inputs = %w[
+  FileInput
+]
+#  CollectionSelectInput
+#  DateTimeInput
+#  GroupedCollectionSelectInput
+#  NumericInput
+#  PasswordInput
+#  RangeInput
+#  StringInput
+#  TextInput
+#]
+ 
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+ 
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.reject{ |klass| klass == "form-control" }
+    end
+  end
+ 
+  Object.const_set(input_type, new_class)
+end
+
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.error_notification_tag = :div
@@ -13,7 +38,7 @@ SimpleForm.setup do |config|
     b.use :label
     b.use :placeholder
     b.use :input
-    b.use :error, wrap_with: { tag: 'div', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'div', class: 'help-block has-error' }
     b.use :hint, wrap_with: { tag: 'div', class: 'help-block' }
   end
   
