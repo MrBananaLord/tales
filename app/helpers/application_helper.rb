2c1a8d930ec9374516bc949911174d5d2622bddc
Jan Bator <jan.bator@gmail.com>
2014-05-16 14:50:15 +0200

    Helper for basic form action container.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
     options[:class] ||= "btn btn-default"
     link_to name, url, options if condition
   end
+  
+  def basic_actions(fields, url)
+    tags = ""
+    tags += fields.button :submit
+    tags += content_tag(:span) do
+      I18n.t("statements.or")
+    end
+    tags += link_to I18n.t("statements.cancel"), url
+    
+    content_tag(:div, class: :actions) do
+      tags.html_safe
+    end.html_safe
+  end
 end
