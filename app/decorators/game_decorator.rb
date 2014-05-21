61ebacf651aedfd224be04898aad18783d713843
Jan Bator <jan.bator@gmail.com>
2014-05-21 11:01:12 +0200

    Show when game is a draft. Changed game show.

--- app/decorators/game_decorator.rb
+++ app/decorators/game_decorator.rb
@@ -4,10 +4,20 @@ class GameDecorator < Draper::Decorator
   def name_with_stars
     h.content_tag :span, class: "game-name with-stars" do
       html = h.content_tag :span, class: 'name' do
-        object.name
+        game_name
       end
       html += h.stars(object.average_mark)
       html.html_safe
+    end
+  end
+  
+  private
+  
+  def game_name
+    if object.published?
+      object.name
+    else
+      "#{object.name} (#{I18n.t("games.decorator.draft").downcase})"
     end
   end
 
