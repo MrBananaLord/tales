f298806d56ef55ec9c347d409f7ab86378f8482c
Jan Bator <jan.bator@gmail.com>
2014-05-21 10:10:15 +0200

    Fixed collection on home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
   def index
     @most_popular_games = GameDecorator.decorate_collection(
-      GamePolicy::Scope.new(current_user, Game.ordered_by_average_mark).
-      resolve.limit(4))
+      Game.published.ordered_by_average_mark.limit(4))
       
-    @latest_games = GameDecorator.decorate_collection(
-      GamePolicy::Scope.new(current_user, Game.latest).resolve.limit(4))
+    @latest_games = GameDecorator.decorate_collection(Game.published.latest.limit(4))
   end
 end
