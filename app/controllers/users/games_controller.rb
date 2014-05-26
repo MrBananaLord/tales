3c5af1d025639b079cf2c0fb73264aa173ab6bb1
Jan Bator <jan.bator@gmail.com>
2014-05-26 15:01:39 +0200

    Sort by latest in games index.

--- app/controllers/users/games_controller.rb
+++ app/controllers/users/games_controller.rb
@@ -2,7 +2,7 @@ class Users::GamesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @games = GameDecorator.decorate_collection(
-      GamePolicy::Scope.new(current_user, @user.games).resolve.
+      GamePolicy::Scope.new(current_user, @user.games.latest).resolve.
       page(params[:page]).per(10))
   end
 end
