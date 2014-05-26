bdaeff5921b67c09e6a8fb886e0e9052df1922c1
Jan Bator <jan.bator@gmail.com>
2014-05-26 22:13:25 +0200

    User aside profile.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @most_popular_games = GameDecorator.decorate_collection(
       Game.published.by_mark.limit(3))
       
-    @latest_games = GameDecorator.decorate_collection(Game.published.latest.limit(4))
+    @latest_games = GameDecorator.decorate_collection(Game.published.latest.limit(2))
     
     @most_popular_authors = User.by_mark.limit(4)
   end
